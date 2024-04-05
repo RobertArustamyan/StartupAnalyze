1a03f5541706562ced8ca79d775712ae6176c4c9
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-04-05 19:51:28 +0400

    Added class in company_industry.py

--- Functions/company_industry.py
+++ Functions/company_industry.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+
 
 class IndustryAnalyzing:
     def __init__(self, dataframe: pd.DataFrame):
@@ -19,7 +19,71 @@ class IndustryAnalyzing:
             industries = [industry.strip().lower() for industry in industries]
             unique_values.update(industries)
         return list(unique_values)
-    def industry_success_distribution(self):
+
+    def _matching_values_from_focus_column(self, focus):
+        mapping_dict = {
+            'many': 'e-commerce',
+            'game': 'music',
+            'targeted marketing': 'email',
+            'enterprise': 'energy',
+            'risk': 'career / job search',
+            'travel planning': 'analytics',
+            'marketing,sales': 'classifieds',
+            'merchandising, marketing': 'transportation',
+            'strategy': 'advertising',
+            'solution providing': 'network / hosting / infrastructure',
+            'information management': 'healthcare',
+            'customer retention, customer feedback': 'finance',
+            'conssumer web': 'telecommunications',
+            'marketing, strategy': 'publishing',
+            'crm, marketing, human resources': 'human resources (hr)',
+            'development, marketing, and administration': 'search',
+            'service': 'media',
+            'research': 'space travel',
+            'web': 'marketing',
+            'server design': 'insurance',
+            'software': 'security',
+            'consumer web': 'cleantech',
+            'curated web': 'software development',
+            'operations': 'pharmaceuticals',
+            'operation': 'deals',
+            'database management': 'crowdfunding',
+            'mobile app development': 'travel',
+            'software service': 'cloud computing',
+            'targeting': 'entertainment',
+            'social media': 'retail',
+            'sales & marketing': 'education',
+            'news': 'gaming',
+            'sales': 'market research',
+            'crm': 'enterprise software',
+            'social network': 'hospitality',
+            'networking': 'mobile',
+            'ecommerce': 'government',
+            'technology': 'social networking',
+            'games': 'food & beverages',
+            'marketing,sales,risk,operations': 'real estate'
+        }
+        if focus in mapping_dict:
+            return mapping_dict[focus]
+        return
+
+    def _filling_na_values(self):
+        values_with_categorize_need = set()
+        industry_names = self._unique_industry_values
+        for index, row in self.df.iterrows():
+            if pd.isna(row['Industry of company']):
+                focus_function = row['Focus functions of company']
+                if isinstance(focus_function, str):
+                    focus_function = focus_function.strip().lower()
+
+                    if focus_function in industry_names:
+                        self.df.at[index, 'Industry of company'] = focus_function
+                    else:
+                        self.df.at[index, 'Industry of company'] = self._matching_values_from_focus_column(focus_function)
+
+
+    def industry_success_distribution(self, pie_count=10, success=True):
+        self._filling_na_values()
         industry_names = self._unique_industry_values
         industry_counts = {}
 
@@ -28,7 +92,7 @@ class IndustryAnalyzing:
         for industry in industry_names:
             industry_counts[industry] = {'Success': 0, 'Failed': 0}
 
-        for index,row in filtered_df.iterrows():
+        for index, row in filtered_df.iterrows():
             industries = row['Industry of company'].split('|')
             status = row['Dependent-Company Status']
             for industry in industries:
@@ -42,15 +106,24 @@ class IndustryAnalyzing:
         industry_df['Success Rate'] = industry_df['Success'] / industry_df['Total']
         industry_df['Failed Rate'] = industry_df['Failed'] / industry_df['Total']
 
-        # Plot pie chart
-        plt.figure(figsize=(10, 6))
-        industry_df['Success Rate'].plot(kind='pie', autopct='%1.1f%%', startangle=90, colors=plt.cm.tab20.colors)
-        plt.title('Success Rate by Industry')
+
+        industry_df = industry_df.sort_values(by='Total', ascending=False).head(pie_count)
+
+        plt.figure(figsize=(8,6))
+
+        if success:
+            industry_df['Success Rate'].plot(kind='pie', autopct='%1.1f%%', startangle=90, colors=plt.cm.tab20.colors)
+            plt.title('Success Rate by Industry')
+        else:
+            industry_df['Failed Rate'].plot(kind='pie', autopct='%1.1f%%', startangle=90, colors=plt.cm.tab20.colors)
+            plt.title('Failed Rate by Industry')
+
+        plt.axis('equal')
         plt.ylabel('')
-        plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
         plt.show()
+
 
 if __name__ == '__main__':
     a = IndustryAnalyzing(
         pd.read_csv(r"C:\Users\Mikayel\PycharmProjects\InternshipTask\Data\data.csv", encoding='latin-1'))
-    print(a.industry_success_distribution())
+    a.industry_success_distribution()
