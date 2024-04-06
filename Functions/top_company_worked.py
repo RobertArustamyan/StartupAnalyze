8383dfb1f60c99dc2d0f3ec79ea834d591f8abd4
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-04-06 09:54:26 +0400

    Modified top_company_worked.py

--- Functions/top_company_worked.py
+++ Functions/top_company_worked.py
@@ -1,6 +1,6 @@
-import pandas as pd
 import numpy as np
-import seaborn as sns
+import pandas as pd
+
 
 class TopCompanyAnalyze:
     def __init__(self, dataframe: pd.DataFrame):
@@ -10,11 +10,33 @@ class TopCompanyAnalyze:
         '''
         self.df = dataframe
 
+    def _preparing_data(self):
+        self.df['Worked in top companies'] = self.df['Worked in top companies'].replace('No Info', np.nan)
+        self.df['Worked in top companies'] = self.df['Worked in top companies'].replace('Yes', True)
+        self.df['Worked in top companies'] = self.df['Worked in top companies'].replace('No', False)
+        self.df.dropna(subset=['Worked in top companies'], inplace=True)
 
+    def analyze_data(self):
+        self._preparing_data()
+
+        success_in_big_company = len(self.df[
+                                         (self.df['Worked in top companies'] == True) & (
+                                                 self.df['Dependent-Company Status'] == 'Success')])
+        total_in_big_company = len(self.df[
+                                       self.df['Worked in top companies'] == True])
+
+        success_without_big_company = len(self.df[
+                                              (self.df['Worked in top companies'] == False) & (
+                                                      self.df['Dependent-Company Status'] == 'Success')])
+        total_without_big_company = len(self.df[
+                                            self.df['Worked in top companies'] == False])
+
+        print("Success rate in a top company -", success_in_big_company / total_in_big_company * 100)
+        print("Success rate out of a top comapny -", success_without_big_company / total_without_big_company * 100)
 
 
 if __name__ == "__main__":
     analyze = TopCompanyAnalyze(
         pd.read_csv(r"C:\Users\Mikayel\PycharmProjects\InternshipTask\Data\data.csv", encoding='latin-1'))
 
-    print(analyze.df[''])
+    analyze.analyze_data()
